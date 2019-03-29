c11107f6888d74fa8df42f2ff485d085e5315594
Qianxiang Ma <931409754@qq.com>
2019-03-30 00:34:51 +0900

    svd fixes

--- include/svd.cuh
+++ include/svd.cuh
@@ -3,11 +3,19 @@
 
 #include <pspl.cuh>
 
+void swap_col (double *col1, double *col2, const int ny, const int ld)
+{
+  for (int i = 0; i < ny; i++)
+  {
+    const double t = col1[i * ld]; col1[i * ld] = col2[i * ld]; col2[i * ld] = t;
+  }
+}
+
 
 int test1 () 
 {
 
-  const int nx = 4, ny = 8;
+  const int nx = 8, ny = 4;
   
   dev_dense <double> *d_VT, *d_A;
 
@@ -19,6 +27,9 @@ int test1 ()
 
   double *A = d_A -> getElements();
   double *VT = d_VT -> getElements();
+
+svd:
+  bool iter = false;
 
   for(int i = 1; i < nx; i++)
   {
@@ -34,10 +45,17 @@ int test1 ()
         s_ij += A[k * nx + i] * A[k * nx + j];
       }
 
-      const double torque = (s_jj - s_ii) / 2 * s_ij;
+      if (s_ii > s_jj) 
+      { 
+        swap_col(&A[i], &A[j], ny, nx); 
+        swap_col(&VT[i], &VT[j], nx, nx);
+        double t = s_ii; s_ii = s_jj; s_jj = t;
+      }
+
+      const double torque = (s_jj - s_ii) / (2.0 * s_ij);
       const double sign_torque = (torque >= 0) ? 1.0 : -1.0;
-      const double t = sign_torque / (abs(torque) + sqrt(1.0 + torque * torque));
-      const double c = 1.0 / (sqrt(1.0 + t * t));
+      const double t = sign_torque / (abs (torque) + sqrt (1.0 + torque * torque));
+      const double c = 1.0 / (sqrt (1.0 + t * t));
       const double s = c * t;
 
       for (int k = 0; k < ny; k++)
@@ -54,11 +72,25 @@ int test1 ()
         VT[k * nx + j] = s * vi_T + c * vj_T;
       }
 
+      if (abs(s_ij) > 1.e-14) iter = true;
+
     }
   }
 
+  if (iter) goto svd;
 
+  d_A -> print();
   d_VT -> print();
+
+  for (int i = 0; i < nx; i++)
+  {
+    double s = 0.0;
+    for (int j = 0; j < ny; j++)
+    { s += A[j * nx + i] * A[j * nx + i]; }
+
+    s = sqrt(s);
+    printf("%d: %e\n", i, s);
+  }
 
   dev_dense <double> *c = d_A -> matrixMultiplication(d_VT -> transpose());
   c -> print();
